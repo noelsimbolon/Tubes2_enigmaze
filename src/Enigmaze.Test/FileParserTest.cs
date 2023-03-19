92fee73ce0d74b934a764296591636e39768fdc4
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-03-19 21:49:44 +0700

    test: add more unit tests for file parser

--- src/Enigmaze.Test/FileParserTest.cs
+++ src/Enigmaze.Test/FileParserTest.cs
@@ -5,14 +5,14 @@ namespace Enigmaze.Test;
 public class FileParserTest
 {
     [Test]
-    public void ParseFile_ValidFile_ReturnsExpectedOutput()
+    public void ParseFile_ValidFile()
     {
         // Arrange
-        char[,] expectedMatrix =
+        var expectedMatrix = new List<List<char>>
         {
-            { 'K', 'R', 'R' },
-            { 'T', 'X', 'T' },
-            { 'R', 'X', 'R' }
+            new List<char>() { 'K', 'R', 'R' },
+            new List<char>() { 'T', 'X', 'T' },
+            new List<char>() { 'R', 'X', 'R' }
         };
 
         const int expectedTreasureCount = 2;
@@ -21,7 +21,7 @@ public class FileParserTest
         var expectedStartingPosition = (0, 0);
 
         // Act
-        (char[,] matrix, (int startingRow, int startingCol), int treasureCount, int rows, int cols) =
+        var (matrix, (startingRow, startingCol), treasureCount, rows, cols) =
             FileParser.ParseFile("../../../../../test/maze1.txt");
 
         // Assert
@@ -32,6 +32,47 @@ public class FileParserTest
             Assert.That(rows, Is.EqualTo(expectedRows));
             Assert.That(cols, Is.EqualTo(expectedCols));
             Assert.That((startingRow, startingCol), Is.EqualTo(expectedStartingPosition));
+        });
+    }
+
+    [Test]
+    public void ParseFile_ExtraSpaces()
+    {
+        // Arrange
+        var expectedMatrix = new List<List<char>>
+        {
+            new List<char>() { 'X', 'X', 'T', 'R', 'R' },
+            new List<char>() { 'T', 'T', 'X', 'K', 'R' },
+            new List<char>() { 'R', 'X', 'X', 'R', 'X' },
+            new List<char>() { 'R', 'R', 'R', 'R', 'X' }
+        };
+
+        const int expectedTreasureCount = 3;
+        const int expectedRows = 4;
+        const int expectedCols = 5;
+        var expectedStartingPosition = (1, 3);
+
+        // Act
+        var (matrix, (startingRow, startingCol), treasureCount, rows, cols) =
+            FileParser.ParseFile("../../../../../test/maze2.txt");
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(matrix, Is.EqualTo(expectedMatrix));
+            Assert.That(treasureCount, Is.EqualTo(expectedTreasureCount));
+            Assert.That(rows, Is.EqualTo(expectedRows));
+            Assert.That(cols, Is.EqualTo(expectedCols));
+            Assert.That((startingRow, startingCol), Is.EqualTo(expectedStartingPosition));
+        });
+    }
+
+    [Test]
+    public void ParseFile_ExtraColumns()
+    {
+        Assert.Throws<InvalidDataException>(() =>
+        {
+            FileParser.ParseFile("../../../../../test/maze3.txt");
         });
     }
 }
