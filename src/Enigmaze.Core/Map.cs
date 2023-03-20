a859b83eda3b7609dc645acb8879de8b589572df
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-03-20 16:13:27 +0700

    refactor: map now use auto properties

--- src/Enigmaze.Core/Map.cs
+++ src/Enigmaze.Core/Map.cs
@@ -4,13 +4,14 @@ namespace Enigmaze.Core;
 
 public class Map
 {
-    private readonly List<List<char>> _matrix;
-    private readonly (int, int) _startingPoint;
-    private readonly int _treasureCount;
-    private readonly int _rows;
-    private readonly int _cols;
+    private List<List<char>> _matrix;
+    private (int, int) _startingPoint;
+    private int _treasureCount;
+    private int _rows;
+    private int _cols;
 
-    public Map(List<List<char>> matrix, (int, int) startingPoint, int treasureCount, int rows, int cols)
+    // Constructor
+    public Map(List<List<char>> matrix, (int x, int y) startingPoint, int treasureCount, int rows, int cols)
     {
         _matrix = matrix;
         _startingPoint = startingPoint;
@@ -19,28 +20,14 @@ public class Map
         _cols = cols;
     }
 
-    public List<List<char>> GetMatrix()
-    {
-        return _matrix;
-    }
-
-    public (int x, int y) GetStartingPoint()
-    {
-        return _startingPoint;
-    }
-
-    public int GetTreasureCount()
-    {
-        return _treasureCount;
-    }
-
-    public int GetRows()
-    {
-        return _rows;
-    }
-
-    public int GetCols()
-    {
-        return _cols;
-    }
+    // Auto properties
+    public List<List<char>> Matrix { get; set; }
+    
+    public (int, int) StartingPoint { get; set; }
+    
+    public int TreasureCount { get; set; }
+    
+    public int Rows { get; set; }
+    
+    public int Cols { get; set; }
 }
