e8149bf3800a73593c26d8a610c59a3b4b2a2c89
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-03-22 03:50:24 +0700

    add: class containing utility method(s)

---
+++ src/Enigmaze.Core/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Enigmaze.Core;
+
+public class Utils
+{
+    public static List<List<bool>> createNewHasVisited(int rows, int cols)
+    {
+        var visitedNodes = new List<List<bool>>();
+        
+        for (int i = 0; i < rows; i++)
+        {
+            var visitedNodesRow = new List<bool>();
+            
+            for (int j = 0; j < cols; j++)
+            {
+                visitedNodesRow.Add(false);
+            }
+            
+            visitedNodes.Add(visitedNodesRow);
+        }
+
+        return visitedNodes;
+    }
+}
