648f6976d718a55da30939d26957ae917fff53c2
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-03-22 00:40:42 +0700

    refactor: Map class
    - Map now uses auto properties
    - add FindKrustyKrab method

--- src/Enigmaze.Core/Map.cs
+++ src/Enigmaze.Core/Map.cs
@@ -4,50 +4,45 @@ namespace Enigmaze.Core;
 
 public class Map
 {
-    private List<List<char>> _matrix;
-    private (int, int) _startingPoint;
-    private int _treasureCount;
-    private int _rows;
-    private int _cols;
+    // Auto properties
+    public List<List<char>> Matrix { get; set; }
+    public (int, int) StartingPoint { get; set; }
+    public int TreasureCount { get; set; }
+    public int Rows { get; set; }
+    public int Cols { get; set; }
 
     // Constructor
     public Map(List<List<char>> matrix, (int x, int y) startingPoint, int treasureCount, int rows, int cols)
     {
-        _matrix = matrix;
-        _startingPoint = startingPoint;
-        _treasureCount = treasureCount;
-        _rows = rows;
-        _cols = cols;
+        Matrix = matrix;
+        StartingPoint = startingPoint;
+        TreasureCount = treasureCount;
+        Rows = rows;
+        Cols = cols;
     }
 
-    // Auto properties
-    public List<List<char>> Matrix
+    // Method to find the index of matrix that contains the character 'K'
+    public (int, int) FindKrustyKrab()
     {
-        get => _matrix;
-        set => _matrix = value;
-    }
-    
-    public (int, int) StartingPoint
-    {
-        get => _startingPoint;
-        set => _startingPoint = value;
-    }
+        // Initialize starting point
+        int startingRow = 0;
+        int startingCol = 0;
+        
+        // Determine the starting point
+        bool shouldBreak = false;
+        for (int i = 0; i < Matrix.Count && !shouldBreak; i++)
+        {
+            for (int j = 0; j < Matrix[0].Count && !shouldBreak; j++)
+            {
+                if (Matrix[i][j] == 'K')
+                {
+                    startingRow = i;
+                    startingCol = j;
+                    shouldBreak = true;
+                }
+            }
+        }
 
-    public int TreasureCount
-    {
-        get => _treasureCount;
-        set => _treasureCount = value;
-    }
-
-    public int Rows
-    {
-        get => _rows;
-        set => _rows = value;
-    }
-
-    public int Cols
-    {
-        get => _cols;
-        set => _cols = value;
+        return (startingRow, startingCol);
     }
 }
